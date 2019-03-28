554589e61eae473eaefb435ae17415cf3d870102
Leopold Talirz <leopold.talirz@gmail.com>
2019-03-28 14:48:19 +0100

    updates from the cookiecutter (#12)
    this commit updates aiida-diff to compatibility with aiida-core 1.0.0 (commit hash 0ca5d991d039dca2261df3d6ad8da2f03d591caa from the develop branch).
    
    * update to aiida-core 1.0
    * revert to original plugin fixtures
    * update readme
    * get and check output
    * enable use of computer workdir
    * remove outdated utils.py
    * show timings of tests

--- aiida_diff/__init__.py
+++ aiida_diff/__init__.py
@@ -4,4 +4,10 @@ aiida_diff
 AiiDA demo plugin that computes the difference between two files.
 """
 
+from __future__ import absolute_import
+
 __version__ = "0.1.0a0"
+
+# disable psycopg2 warning
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module='psycopg2')
--- aiida_diff/tests/test_calculations.py
+++ aiida_diff/tests/test_calculations.py
@@ -9,47 +9,51 @@ import aiida_diff.tests as tests
 import pytest
 
 
-# pylint: disable=unused-argument
+# pylint: disable=unused-argument,too-many-locals
 @pytest.mark.process_execution
 def test_process(new_database, new_workdir):
     """Test running a calculation
     note this does not test that the expected outputs are created of output parsing"""
-    from aiida.orm.data.singlefile import SinglefileData
+    from aiida.plugins import DataFactory, CalculationFactory
+    from aiida.engine import run_get_node
 
     # get code
-    code = tests.get_code(entry_point='diff')
+    computer = tests.get_computer(workdir=new_workdir)
+    code = tests.get_code(entry_point='diff', computer=computer)
 
     # Prepare input parameters
-    from aiida.orm import DataFactory
     DiffParameters = DataFactory('diff')
     parameters = DiffParameters({'ignore-case': True})
 
+    from aiida.orm import SinglefileData
     file1 = SinglefileData(
         file=os.path.join(tests.TEST_DIR, "input_files", 'file1.txt'))
     file2 = SinglefileData(
         file=os.path.join(tests.TEST_DIR, "input_files", 'file2.txt'))
 
     # set up calculation
-    calc = code.new_calc()
-    calc.label = "aiida_diff test"
-    calc.description = "Test job submission with the aiida_diff plugin"
-    calc.set_max_wallclock_seconds(30)
-    calc.set_withmpi(False)
-    calc.set_resources({"num_machines": 1, "num_mpiprocs_per_machine": 1})
+    options = {
+        "resources": {
+            "num_machines": 1,
+            "num_mpiprocs_per_machine": 1,
+        },
+        "max_wallclock_seconds": 30,
+    }
 
-    calc.use_parameters(parameters)
-    calc.use_file1(file1)
-    calc.use_file2(file2)
+    inputs = {
+        'code': code,
+        'parameters': parameters,
+        'file1': file1,
+        'file2': file2,
+        'metadata': {
+            'options': options,
+            'label': "aiida_diff test",
+            'description': "Test job submission with the aiida_diff plugin",
+        },
+    }
 
-    calc.store_all()
+    _result, node = run_get_node(CalculationFactory('diff'), **inputs)
 
-    ## output input files and scripts to temporary folder
-    #from aiida.common.folders import SandboxFolder
-    #with SandboxFolder() as folder:
-    #    subfolder, script_filename = calc.submit_test(folder=folder)
-    #    print("inputs created successfully at {0} with script {1}".format(
-    #        subfolder.abspath, script_filename))
-    # test process execution and check the expected outputs
-    # for diff 0=no differences, 1=differences, >1=error
-    tests.test_calculation_execution(
-        calc, allowed_returncodes=(1, ), check_paths=[calc._OUTPUT_FILE_NAME])  # pylint: disable=protected-access
+    computed_diff = node.outputs.diff.get_content()
+    assert 'content1' in computed_diff
+    assert 'content2' in computed_diff
