92c61bdcc2db201d69da4d8b83a2b3f5dd529bf1
Leopold Talirz <leopold.talirz@gmail.com>
2020-06-29 14:29:57 +0200

    update developer docs (#36)
     * fix build on readthedocs
     * fix installation bug (aiida_diff.data package not found!!)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     with open('setup.json', 'r') as info:
         kwargs = json.load(info)
     setup(
-        packages=find_packages(),
+        packages=find_packages(exclude=['tests*']),
         # this doesn't work when placed in setup.json (something to do with str type)
         package_data={
             "": ["*"],
