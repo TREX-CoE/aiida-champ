8f8ea0e1f54c0a2b8a97674e6e103ecb6344ba1a
Ravindra Shinde <neelravi@gmail.com>
2021-12-26 17:29:56 +0100

    tests modified

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -2,39 +2,73 @@
 """ Tests for calculations
 
 """
-import os
+from os import path
 from aiida.plugins import DataFactory, CalculationFactory
-from aiida.engine import run
-from aiida.orm import SinglefileData
+from aiida import engine
+from aiida_champ import helpers
 
-from . import TEST_DIR
+INPUT_DIR = path.join(path.dirname(path.realpath(__file__)), 'test01')
+POOL_DIR = path.join(path.dirname(path.realpath(__file__)), 'test01/pool')
 
 
-def test_process(diff_code):
-    """Test running a calculation
-    note this does not test that the expected outputs are created of output parsing"""
+def test_process(champ_code):
+    """Run a calculation on the localhost computer.
 
-    # Prepare input parameters
-    DiffParameters = DataFactory('vmc_mov1')
-    parameters = DiffParameters({})
+    Uses test helpers to create AiiDA Code on the fly.
+    """
+    if not champ_code:
+        # get code
+        computer = helpers.get_computer()
+        champ_code = helpers.get_code(entry_point='CHAMP', computer=computer)
 
-    file1 = SinglefileData(
-        file=os.path.join(TEST_DIR, 'input_files', 'vmc.inp'))
+
+    SinglefileData = DataFactory('singlefile')
+    # RemoteData = DataFactory('remotedata')
+
+    filemain = SinglefileData(file=path.join(INPUT_DIR, 'vmc.inp'))
+    molecule = SinglefileData(file=path.join(INPUT_DIR, 'butadiene.xyz'))
+
+    # pooldir = RemoteData(file=POOL_DIR)
+
+    ecp1 = SinglefileData(file=path.join(INPUT_DIR, 'BFD.gauss_ecp.dat.C'))
+    ecp2 = SinglefileData(file=path.join(INPUT_DIR, 'BFD.gauss_ecp.dat.H'))
+
+    orbitals = SinglefileData(file=path.join(INPUT_DIR, 'cas44.lcao'))
+    determinants = SinglefileData(file=path.join(INPUT_DIR, 'cas44.det'))
+    symmetry = SinglefileData(file=path.join(INPUT_DIR, 'cas44.sym'))
+    jastrow = SinglefileData(file=path.join(INPUT_DIR, 'jastrow_good_b3lyp.0'))
+    jastrowder = SinglefileData(file=path.join(INPUT_DIR, 'jastrow.der'))
+    numericalbasisinfo = SinglefileData(file=path.join(POOL_DIR, 'BFD-Q.bfinfo'))
+    numericalbasis1 = SinglefileData(file=path.join(INPUT_DIR, 'BFD-Q.basis.C'))
+    numericalbasis2 = SinglefileData(file=path.join(INPUT_DIR, 'BFD-Q.basis.H'))
+
 
     # set up calculation
     inputs = {
-        'code': diff_code,
-        'parameters': parameters,
-        'file1': file1,
+        'code': champ_code,
+        'filemain': filemain,
+        'molecule': molecule,
+        # 'pooldir': pooldir,
+        'ecp1': ecp1,
+        'ecp2': ecp2,
+        'orbitals': orbitals,
+        'determinants': determinants,
+        'symmetry': symmetry,
+        'jastrow': jastrow,
+        'jastrowder': jastrowder,
+        'numericalbasisinfo': numericalbasisinfo,
+        'numericalbasis1': numericalbasis1,
+        'numericalbasis2': numericalbasis2,
         'metadata': {
-            'options': {
-                'max_wallclock_seconds': 300
-            },
+            'description': 'Sample job submission with the aiida_champ plugin example 01',
         },
     }
 
-    result = run(CalculationFactory('vmc_mov1'), **inputs)
-    computed_diff = result['vmc_mov1'].get_content()
+    # Note: in order to submit your calculation to the aiida daemon, do:
+    result = engine.run(CalculationFactory('CHAMP'), **inputs)
+    computed_diff = result['Output'].get_content()
+    print('Outout of the Calculation: \n{}'.format(computed_diff))
 
-    # assert 'content1' in computed_diff
-    # assert 'content2' in computed_diff
+    assert 'total E' in computed_diff
+    assert 'Total time of computation' in computed_diff
+
